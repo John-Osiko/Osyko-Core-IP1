030408e677a2a9597ecaa63a177e83dd8d1179ce
John-Osiko <johnmaxosiko@gmail.com>
2020-08-26 12:27:22 +0300

    Added more components and a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteComponent } from './quote/quote.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
-    QuoteComponent
+    QuoteComponent,
+    DateCountPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,

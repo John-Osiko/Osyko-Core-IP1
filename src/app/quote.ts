b822099a33073f5b78e54c85ec94b699f3f49890
John-Osiko <johnmaxosiko@gmail.com>
2020-08-27 08:26:43 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    showDescription:boolean;
-    constructor(public id: number, public title: string, public name: string, public description: string, public poster: string, public submitDate:Date){
-        this.showDescription = false;
-    }
-    
-}
